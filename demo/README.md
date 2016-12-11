1864fb3dcd70cbd15fd81c1869011026f8980ba2
xR86 <9412441+xR86@users.noreply.github.com>
2016-12-12 01:39:04 +0200

    updated libssh demo

--- demo/README.md
+++ demo/README.md
@@ -8,11 +8,13 @@ Computer Networks Project
 + `libssh/` - using a openssh-compliant library for C
   + follow install guide here: [official](https://github.com/substack/libssh/blob/master/INSTALL#L26-L29), [issue](https://askubuntu.com/questions/741351/trying-to-install-libssh-0-7-x-on-ubuntu)
   + tutorial [here](http://api.libssh.org/master/libssh_tutor_guided_tour.html)
-  + configure openssh-server ([here](https://askubuntu.com/questions/51925/how-do-i-configure-a-new-ubuntu-installation-to-accept-ssh-connections), [extended](https://thepcspy.com/read/making-ssh-secure/)):
+  + configure openssh-server for client machines ([here](https://askubuntu.com/questions/51925/how-do-i-configure-a-new-ubuntu-installation-to-accept-ssh-connections), [extended](https://thepcspy.com/read/making-ssh-secure/)):
     + `sudo apt-get update`
 	+ `sudo apt-get install openssh-server` (implicitly installs `openssh-client`, if **not** add it to the command)
 	+ `sudo ufw allow 22`
 	+ configure - [quick](https://help.ubuntu.com/14.04/serverguide/openssh-server.html), [detailed](https://help.ubuntu.com/community/SSH/OpenSSH/Configuring) (you can check the files location with `dpkg -L libssh-dev`)
 	+ `ssh-keygen -t rsa`
 	+ `ssh-copy-id remoteuser@remotehostIP` - will add the id to known hosts
+  + `/etc/ssh/sshd_config/`
+  + `sudo service ssh restart`
 
---
+++ demo/libssh/libssh-example.c
@@ -0,0 +1,71 @@
+#include <libssh/libssh.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void free_channel(ssh_channel channel) {
+    ssh_channel_send_eof(channel);
+    ssh_channel_close(channel);
+    ssh_channel_free(channel);
+}
+
+void free_session(ssh_session session) {
+    ssh_disconnect(session);
+    ssh_free(session);
+}
+
+void error(ssh_session session) {
+    fprintf(stderr, "Error: %s\n", ssh_get_error(session));
+    free_session(session);
+    exit(-1);
+}
+
+int main() {
+
+    ssh_session session;
+    ssh_channel channel;
+    int rc, port = 22;
+    char buffer[1024];
+    unsigned int nbytes;
+
+    printf("Session...\n");
+    session = ssh_new();
+    if (session == NULL) exit(-1);
+
+    ssh_options_set(session, SSH_OPTIONS_HOST, "localhost"); //localhost //192.168.100.2
+    ssh_options_set(session, SSH_OPTIONS_PORT, &port);
+    ssh_options_set(session, SSH_OPTIONS_USER, "xR86"); //xR86 //labsi
+
+    printf("Connecting...\n");
+    rc = ssh_connect(session);
+    if (rc != SSH_OK) error(session);
+
+    printf("Password Autentication...\n");
+    //rc = ssh_userauth_password(session, "NULL", "xR86");
+    //rc = ssh_userauth_password(session, NULL, "parola");
+    rc = ssh_userauth_publickey_auto(session, "xR86", NULL);
+    if (rc != SSH_AUTH_SUCCESS) error(session);
+
+    printf("Channel...\n");
+    channel = ssh_channel_new(session);
+    if (channel == NULL) exit(-1);
+
+    printf("Opening...\n");
+    rc = ssh_channel_open_session(channel);
+    if (rc != SSH_OK) error(session);
+
+    printf("Executing remote command...\n");
+    rc = ssh_channel_request_exec(channel, "ls -l");
+    if (rc != SSH_OK) error(session);
+
+    printf("Received:\n");
+    nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+    while (nbytes > 0) {
+        fwrite(buffer, 1, nbytes, stdout);
+        nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+    }
+
+    free_channel(channel);
+    free_session(session);
+
+    return 0;
+}
